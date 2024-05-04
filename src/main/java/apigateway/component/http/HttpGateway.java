6dd696f0bd7b79b6de27a152dd8811bbf053c19a
junzmint <junzmint07112002@gmail.com>
2024-05-04 11:41:24 +0700

    Connect kafka producer

--- src/main/java/apigateway/component/http/HttpGateway.java
+++ src/main/java/apigateway/component/http/HttpGateway.java
@@ -1,10 +1,23 @@
 package apigateway.component.http;
 
+import apigateway.component.ApiGatewayConstants;
+import apigateway.component.kafkaproducer.KafkaProducer;
+import apigateway.component.kafkaproducer.KafkaProducerConfig;
 import io.gridgo.core.GridgoContext;
 import io.gridgo.core.support.RoutingContext;
-import io.gridgo.framework.support.Message;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 public class HttpGateway extends HttpGatewayBaseComponent {
+    private final KafkaProducerConfig config = new KafkaProducerConfig(
+            ApiGatewayConstants.getKafkaBroker(),
+            StringSerializer.class.getName(),
+            StringSerializer.class.getName(),
+            "1",
+            ApiGatewayConstants.getKafkaTopic(),
+            0);
+
+    private final KafkaProducer producer = new KafkaProducer(config);
+
     public HttpGateway(String gatewayName) {
         super(gatewayName);
     }
@@ -13,6 +26,7 @@ public class HttpGateway extends HttpGatewayBaseComponent {
         var msg = rc.getMessage();
         var deferred = rc.getDeferred();
 
-        deferred.resolve(Message.ofAny(msg.body()));
+        producer.send(msg, null);
+        // deferred.resolve(Message.ofAny(msg.body()));
     }
 }
