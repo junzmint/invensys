011dbc7fd3ec59fee9c9b1abab6921bd321dd3e5
junzmint711 <junzmint07112002@gmail.com>
2024-05-29 11:34:46 +0700

    fea: using threads pool to seed data

--- src/main/java/database/seeder/Seeder.java
+++ src/main/java/database/seeder/Seeder.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Seeder {
     private static final Random random = new Random();
@@ -26,32 +29,60 @@ public class Seeder {
     }
 
     public static void seed(Long numberOfRecords) {
-        DatabaseConnector databaseConnector = DatabaseConnector.databaseConnectorFactory();
-
-        Connection databaseConnection = databaseConnector.databaseConnect();
-        DatabaseQueryExecutor databaseQueryExecutor = new DatabaseQueryExecutor(databaseConnection);
-
-        seedInventoryTable(databaseQueryExecutor, numberOfRecords);
-        seedOffsetTable(databaseQueryExecutor);
-        databaseQueryExecutor.close();
+        seedInventoryTable(numberOfRecords);
+        seedOffsetTable();
     }
 
-    public static void seedInventoryTable(DatabaseQueryExecutor databaseQueryExecutor, long numberOfRecords) {
-        Map<String, Long> inventoryBatch = new HashMap<>();
-        for (long sku = 0; sku < numberOfRecords; sku++) {
-            UUID uuid = UUID.randomUUID();
-            String skuId = uuid.toString().substring(0, 4);
-            long quantity = random.nextInt(1000) + 1;
-            inventoryBatch.put(skuId, quantity);
+    public static void seedInventoryTable(long numberOfRecords) {
+        int numThreads = 10;
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+        List<Callable<Void>> tasks = new ArrayList<>();
+
+        long recordsPerThread = numberOfRecords / numThreads;
+        
+        for (int i = 0; i < numThreads; i++) {
+            long start = i * recordsPerThread;
+            long end = (i == numThreads - 1) ? numberOfRecords : (i + 1) * recordsPerThread;
+            tasks.add(() -> {
+                DatabaseConnector databaseConnector = DatabaseConnector.databaseConnectorFactory();
+                Connection databaseConnection = databaseConnector.databaseConnect();
+                DatabaseQueryExecutor databaseQueryExecutor = new DatabaseQueryExecutor(databaseConnection);
+
+                Map<String, Long> inventoryBatch = new HashMap<>();
+                for (long sku = start; sku < end; sku++) {
+                    UUID uuid = UUID.randomUUID();
+                    String skuId = uuid.toString();
+                    long quantity = random.nextInt(1000) + 1;
+                    inventoryBatch.put(skuId, quantity);
+                }
+                databaseQueryExecutor.insertInventoryTable(inventoryBatch);
+
+                databaseQueryExecutor.close();
+
+                return null;
+            });
         }
 
-        databaseQueryExecutor.insertInventoryTable(inventoryBatch);
+        try {
+            executor.invokeAll(tasks);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            executor.shutdown();
+        }
+
         DatabaseLogger.logDatabaseInfo("INVENTORY_SEEDED", Thread.currentThread().getStackTrace());
     }
 
-    public static void seedOffsetTable(DatabaseQueryExecutor databaseQueryExecutor) {
+    public static void seedOffsetTable() {
+        DatabaseConnector databaseConnector = DatabaseConnector.databaseConnectorFactory();
+        Connection databaseConnection = databaseConnector.databaseConnect();
+        DatabaseQueryExecutor databaseQueryExecutor = new DatabaseQueryExecutor(databaseConnection);
+
         databaseQueryExecutor.insertOffsetTable("MaxOffset", (long) -1);
 
         DatabaseLogger.logDatabaseInfo("OFFSET_SEEDED", Thread.currentThread().getStackTrace());
+
+        databaseQueryExecutor.close();
     }
 }
